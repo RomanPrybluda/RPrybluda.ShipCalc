6d01cca027accec5f7345688a7ae4595f59e1aaf
Roman Prybluda <romanodua@gmail.com>
2025-05-28 00:22:49 +0300

    add seed data initializer interface

---
+++ src/ShipCalc.Application/Abstractions/Data/ISeedDataInitializer.cs
@@ -0,0 +1,18 @@
+﻿namespace ShipCalc.Application.Abstractions.Data
+{
+    public interface ISeedDataInitializer
+    {
+        Task InitializeRICapacityIceStrengthCorrFactorAsync();
+
+        Task InitializeIASuperAndIAIceCorrFactorAsync();
+
+        Task InitializeRatingThresholdsAsync();
+
+        Task InitializeIReductionFactorAsync();
+
+        Task InitializeIRefDesignBlockCoeffAsync();
+
+        Task InitializeRefLineParamsAsync();
+
+    }
+}
