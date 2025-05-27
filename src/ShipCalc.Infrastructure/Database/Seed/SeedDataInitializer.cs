2f36ba4de45517c086c5c4519fbc3f289cb34887
Roman Prybluda <romanodua@gmail.com>
2025-05-28 01:52:03 +0300

    add seed data creating for one entity

---
+++ src/ShipCalc.Infrastructure/Database/Seed/SeedDataInitializer.cs
@@ -0,0 +1,84 @@
+﻿using ShipCalc.Application.Abstractions.Data;
+using ShipCalc.Application.SeedData;
+
+namespace ShipCalc.Infrastructure.Database;
+
+public class SeedDataInitializer : ISeedDataInitializer
+{
+    private readonly ShipCalcDbContext _context;
+
+    public SeedDataInitializer(ShipCalcDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task InitializeRICapacityIceStrengthCorrFactorAsync()
+    {
+        throw new NotImplementedException();
+
+        //if (!_context.CapacityIceStrengthCorrFactors.Any())
+        //{
+        //    var data = CapacityIceStrengthCorrFactorsSeedData.GetData();
+        //    _context.CapacityIceStrengthCorrFactors.AddRange(data);
+        //    await _context.SaveChangesAsync();
+        //}
+    }
+
+    public async Task InitializeIASuperAndIAIceCorrFactorAsync()
+    {
+        throw new NotImplementedException();
+
+        //if (!_context.IASuperAndIAIceCorrFactors.Any())
+        //{
+        //    var data = IASuperAndIAIceCorrFactorsSeedData.GetData();
+        //    _context.IASuperAndIAIceCorrFactors.AddRange(data);
+        //    await _context.SaveChangesAsync();
+        //}
+    }
+
+    public async Task InitializeRatingThresholdsAsync()
+    {
+        if (!_context.CIIRatingThresholds.Any())
+        {
+            var data = RatingThresholdsSeedData.GetData();
+            _context.CIIRatingThresholds.AddRange(data);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task InitializeIReductionFactorAsync()
+    {
+        throw new NotImplementedException();
+
+        //if (!_context.ReductionFactors.Any())
+        //{
+        //    var data = ReductionFactorsSeedData.GetData();
+        //    _context.ReductionFactors.AddRange(data);
+        //    await _context.SaveChangesAsync();
+        //}
+    }
+
+    public async Task InitializeIRefDesignBlockCoeffAsync()
+    {
+        throw new NotImplementedException();
+
+        //if (!_context.RefDesignBlockCoefficients.Any())
+        //{
+        //    var data = RefDesignBlockCoeffSeedData.GetData();
+        //    _context.RefDesignBlockCoefficients.AddRange(data);
+        //    await _context.SaveChangesAsync();
+        //}
+    }
+
+    public async Task InitializeRefLineParamsAsync()
+    {
+        throw new NotImplementedException();
+
+        //if (!_context.RefLineParams.Any())
+        //{
+        //    var data = RefLineParamsSeedData.GetData();
+        //    _context.RefLineParams.AddRange(data);
+        //    await _context.SaveChangesAsync();
+        //}
+    }
+}
