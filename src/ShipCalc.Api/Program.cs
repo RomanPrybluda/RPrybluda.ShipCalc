bff22b0ad669eb08706c1aa43d544348b6f41510
Roman Prybluda <romanodua@gmail.com>
2025-05-28 01:53:18 +0300

    add dependency injection

--- src/ShipCalc.Api/Program.cs
+++ src/ShipCalc.Api/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
-using ShipCalc.Infrastructure.Data;
+using ShipCalc.Infrastructure;
+using ShipCalc.Infrastructure.Database;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +20,13 @@ if (string.IsNullOrWhiteSpace(connectionString))
     throw new InvalidOperationException("Connection string is not set. Check environment variables, appsettings.json, or secrets.");
 }
 
+builder.Services.AddInfrastructure();
+
+//builder.Services
+//    .AddApplication()
+//    .AddPresentation()
+//    .AddInfrastructure(builder.Configuration);
+
 builder.Services.AddDbContext<ShipCalcDbContext>(options =>
     options.UseNpgsql(connectionString, npgsqlOptions =>
         npgsqlOptions.MigrationsHistoryTable(HistoryRepository.DefaultTableName, "Default"))
@@ -27,6 +36,25 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var context = services.GetRequiredService<ShipCalcDbContext>();
+        context.Database.Migrate();
+
+        var runner = scope.ServiceProvider.GetRequiredService<SeedDataRunner>();
+        await runner.RunAllAsync();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred during database initialization.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
---
+++ src/ShipCalc.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ShipCalc.Application.Abstractions.Data;
+using ShipCalc.Infrastructure.Database;
+
+namespace ShipCalc.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+
+        //services.AddDbContext<ShipCalcDbContext>();
+        services.AddScoped<ISeedDataInitializer, SeedDataInitializer>();
+        services.AddScoped<SeedDataRunner>();
+
+        return services;
+    }
+}
