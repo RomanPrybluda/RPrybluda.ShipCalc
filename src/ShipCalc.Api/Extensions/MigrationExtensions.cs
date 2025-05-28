64c20933228521c590aca853b6965afa9e8f83bf
Roman Prybluda <romanodua@gmail.com>
2025-05-28 11:45:21 +0300

    add migration and seed data extention

---
+++ src/ShipCalc.Api/Extensions/MigrationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShipCalc.Infrastructure.Database;
+
+namespace ShipCalc.Api.Extensions;
+
+public static class MigrationExtensions
+{
+    public static async Task InitializeDatabaseAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var services = scope.ServiceProvider;
+
+        try
+        {
+            var context = services.GetRequiredService<ShipCalcDbContext>();
+            await context.Database.MigrateAsync();
+
+            var runner = services.GetRequiredService<SeedDataRunner>();
+            await runner.RunAllAsync();
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred during database initialization.");
+        }
+    }
+}
--- src/ShipCalc.Api/Program.cs
+++ src/ShipCalc.Api/Program.cs
@@ -1,67 +1,21 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations;
+using ShipCalc.Api.Extensions;
 using ShipCalc.Infrastructure;
-using ShipCalc.Infrastructure.Database;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("Default");
-
-var localConnectionString = builder.Configuration["ConnectionStrings:LocalConnectionString"];
-
-if (!string.IsNullOrWhiteSpace(localConnectionString))
-{
-    connectionString = localConnectionString;
-}
-
-if (string.IsNullOrWhiteSpace(connectionString))
-{
-    throw new InvalidOperationException("Connection string is not set. Check environment variables, appsettings.json, or secrets.");
-}
-
-builder.Services.AddInfrastructure();
-
-//builder.Services
-//    .AddApplication()
-//    .AddPresentation()
-//    .AddInfrastructure(builder.Configuration);
-
-builder.Services.AddDbContext<ShipCalcDbContext>(options =>
-    options.UseNpgsql(connectionString, npgsqlOptions =>
-        npgsqlOptions.MigrationsHistoryTable(HistoryRepository.DefaultTableName, "Default"))
-           .UseSnakeCaseNamingConvention());
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
+await app.InitializeDatabaseAsync();
 
-    try
-    {
-        var context = services.GetRequiredService<ShipCalcDbContext>();
-        context.Database.Migrate();
-
-        var runner = scope.ServiceProvider.GetRequiredService<SeedDataRunner>();
-        await runner.RunAllAsync();
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred during database initialization.");
-    }
-}
-
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
 app.UseSwagger();
 app.UseSwaggerUI();
-//}
 
 app.UseHttpsRedirection();
 
